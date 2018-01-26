31a7764d9359a7716f352e0a2c89c5874723cb63
alesm97 <32259637+alesm97@users.noreply.github.com>
2018-01-26 09:11:56 +0100

    26/01/2018      9:11

---
+++ PinBall/Assets/Scripts/FlipperScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour {
+
+    //Ángulo de poscion en descanso
+    public float restPosition = 0f;
+    //Ángulo de poscion presionado
+    public float pressedPosition = 45f;
+    //fuerza del golpe del impacto
+    public float hitStrength = 10000f;
+    // velocidad del flipper
+    public float flipperDamper = 150f;
+    // nombre de la entrada
+    public string inputName;
+
+    HingeJoint hingejoint;
+
+	// Use this for initialization
+	void Start () {
+        hingejoint = GetComponent<HingeJoint>();
+        hingejoint.useSpring = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+		if(Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+
+        hingejoint.spring = spring;
+        hingejoint.useLimits = true;
+	}
+}
